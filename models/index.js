3ed621136df20e9752ca584089ef81acc3468291
cmdnguyen <cmdnguyen94@gmail.com>
2023-07-31 21:45:48 -0500

    Worked on the models and added placeholders for README

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+})
 // Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 
 module.exports = {
   Product,
